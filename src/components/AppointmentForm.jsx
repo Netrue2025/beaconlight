4a7ff4d512d72e52dcd90ca2ff3a68fd89b3d3fa
haybdev <officewebb01@gmail.com>
2025-11-25 15:20:23 +0100

    AppointmentForm component completed

--- src/components/AppointmentForm.jsx
+++ src/components/AppointmentForm.jsx
@@ -1,21 +1,19 @@
+import "../styles/appointmentform.css";
+
 export default function AppointmentForm() {
-return (
-    <section className="px-6 lg:px-24 py-20">
-    <div className="max-w-xl mx-auto bg-white p-8 rounded-xl shadow">
-    <h2 className="text-3xl font-bold text-slate-900 mb-6 text-center">Book Appointment</h2>
-    <form className="flex flex-col gap-4">
-    <input className="border p-3 rounded" placeholder="Full Name" />
-    <input className="border p-3 rounded" placeholder="Phone Number" />
-    <select className="border p-3 rounded">
-    <option>Choose Department</option>
-    <option>Cardiology</option>
-    <option>Pediatrics</option>
-    <option>Diagnostics</option>
-    </select>
-    <input className="border p-3 rounded" type="date" />
-    <button className="bg-blue-600 text-white p-3 rounded-lg">Submit</button>
-    </form>
-    </div>
+  return (
+    <section className="appointment-section px-6 lg:px-24 py-20">
+      <div className="inner-cont flex flex-col gap-6 justify-center w-[600px] items-center m-auto">
+        <h1 className="text-gray-900 text-center text-3xl font-bold leading-tight tracking-tighter sm:text-4xl">
+          Ready for Your Visit? Book an Appointment Today
+        </h1>
+        <p className="max-w-2xl mx-auto text-lg text-gray-600 dark:text-gray-400">
+          Booking an appointment is quick and easy. Let us take care of you.
+        </p>
+        <button className="btn1 flex min-w-[84px] cursor-pointer items-center justify-center overflow-hidden rounded-lg h-12 px-5 bg-primary text-white text-base font-bold leading-normal tracking-wide hover:bg-opacity-90 transition-colors">
+          <span className="truncate">Book an Appointment</span>
+        </button>
+      </div>
     </section>
-);
+  );
 }
