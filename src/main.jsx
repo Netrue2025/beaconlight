0d2d3e88c8445a5b78ed7cba00364708243fa7cd
juba <juba.jeremiah@gmail.com>
2025-11-25 23:29:01 +0000

    Footer completed

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import AppointmentForm from './components/AppointmentForm.jsx'
+import { HashRouter as Router, Routes, Route } from "react-router-dom"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+        <Route path="/Appointment" element={<AppointmentForm />}></Route>
+      </Routes>
+    </Router>
+    
   </StrictMode>,
 )
