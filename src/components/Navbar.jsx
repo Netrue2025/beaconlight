bc30429d20614f42441b5ec9fe6ecee85f204201
juba <juba.jeremiah@gmail.com>
2025-11-27 16:14:08 +0000

    color corrections

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,13 +10,13 @@ export default function Navbar() {
             <header className="sticky top-0 z-50 bg-background-light/80 dark:bg-background-dark/80 backdrop-blur-sm">
                 <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="inner-content flex items-center justify-between h-16 border-b border-gray-200 dark:border-gray-700">
-                        <div className="flex items-center gap-1 text-gray-800 dark:text-white">
+                        <div className="header-con flex items-center gap-1 text-gray-800 dark:text-white">
                         
-                        <div className="mobile-menu" onClick={()=> showmenu === false ? setShowmenu(true): setShowmenu(false)}>
-                           <img src="/images/menu-icon.png" alt="Menu Icon" /> 
-                        </div>
-                        <img src="/images/logo.png" alt="Logo" className="h-14 w-22 text-primary"/>
-                        <h1 className="text-xl font-bold leading-tight tracking-tighter">Beaconlight Hospital</h1>
+                            <div className="mobile-menu" onClick={()=> showmenu === false ? setShowmenu(true): setShowmenu(false)}>
+                            <img src="/images/menu-icon.png" alt="Menu Icon" /> 
+                            </div>
+                            <img src="/images/logo.png" alt="Logo" className="h-14 w-22 text-primary"/>
+                            <h1 id="h1" className="text-xl font-bold leading-tight tracking-tighter">Beaconlight Hospital</h1>
                         </div>
                         <nav className="hidden md:flex items-center gap-8">
                         {/* <a className="text-sm font-medium text-gray-700 dark:text-gray-300 hover:text-primary dark:hover:text-primary transition-colors" href="#">Home</a> */}
